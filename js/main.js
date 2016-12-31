218cc9a39c9c19877e15d49ae1b455e536f3f7ec
plrin <ductan.dang@gmail.com>
2016-12-31 16:49:54 +0100

    added buttons

--- js/main.js
+++ js/main.js
@@ -2,11 +2,33 @@ $(function() {
     
     new TodoApp(
         {
-            input : $(".input-field"),
-            list : $(".list-container__list"),
-            closeButton : $(".icon-close"),
-            close : ".icon-close"
+            input       : $(".input-field"),
+            listNormal  : $(".list-container__normal"),
+            listFinished: $(".list-container__finished"),
+            listDeleted : $(".list-container__deleted"),
+            close       : ".icon-close",
+            done        : ".icon-done",
+            btnAll      : $(".button-all"),
+            btnFinished : $(".button-finished"),
+            btnDelete   : $(".button-deleted")
         }
     );
     
 });
+
+function maxMultiple(divisor, bound) {
+    
+    var result;
+    
+    for(i = bound; i > 0; i--) {
+        var tmp = i % divisor;
+        if (tmp == 0) {
+            result = i;
+            break
+        }
+        console.log(i + " : " + tmp);
+    }
+    
+    return result;
+    
+}
--- js/todo.lib.js
+++ js/todo.lib.js
@@ -1,9 +1,14 @@
 function TodoApp($el) {
-    var _self = this,
-        $input = $el.input,
-        $list = $el.list,
-        $close = $el.closeButton,
-        close = $el.close;
+    var _self       = this,
+        $input      = $el.input,
+        $lsNormal      = $el.listNormal,
+        $lsFinished  = $el.listFinished,
+        $lsDel      = $el.listDeleted,
+        close       = $el.close,
+        done        = $el.done,
+        $btnAll      = $el.btnAll,
+        $btnFinished  = $el.btnFinished,
+        $btnDelete  = $el.btnDelete;
     
     init();
     
@@ -15,12 +20,14 @@ function TodoApp($el) {
     function addItem() {
         var value = $input.val(),
             html = '<li class="list-container__list-item">' +
-                '<span class="icon-done"><input type="checkbox" class="icon-done__checkbox"></span>' +
+                '<span class="icon-done"></span>' +
                 '<span class="list-container__item-text">' + value +'</span>' +
                 '<span class="icon-close"></span>' +
                 '</li>';
-    
-        $list.append(html);
+        
+        if (value.trim().length > 0)
+            $lsNormal.append(html);
+        
         $input.val("");
     }
     
@@ -28,16 +35,51 @@ function TodoApp($el) {
         item.parent().remove();
     }
     
-    // EVENT LISTENER
+    function markedAsDone(item) {
+        if (item.hasClass("done"))
+            item.removeClass("done");
+        else
+            item.addClass("done");
+    }
+    
+    function showList(button) {
+        var attr = button.data("list"),
+            cssClass = ".list-container__" + attr;
+        $lsNormal.removeClass("show");
+        $lsFinished.removeClass("show");
+        $lsDel.removeClass("show");
+        
+        console.log(cssClass);
+        
+        $(".list-container__" + attr).addClass("show");
+    }
+    
+    // region EVENT LISTENER
     $input.on("keydown", function (e) {
         if(e.which == 13) {
             addItem();
         }
     });
     
-    $(document).on("click", ".icon-close", function () {
+    $(document).on("click", close, function() {
         removeItem($(this));
     });
     
+    $(document).on("click", done, function() {
+        markedAsDone($(this));
+    });
+
+    $btnAll.on("click", function() {
+        showList($(this));
+    });
+
+    $btnFinished.on("click", function() {
+        showList($(this));
+    });
+
+    $btnDelete.on("click", function() {
+        showList($(this));
+    });
+    // endregion
     
 }
